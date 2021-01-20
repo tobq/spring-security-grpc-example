f487af0963b06a00f292ebc4dd39e75b58fb4a6f
tobi <tobi.akiny@gmail.com>
2021-01-20 15:37:03 +0000

    added log

--- src/main/java/org/akinyemi/tobi/example/services/ExampleService.java
+++ src/main/java/org/akinyemi/tobi/example/services/ExampleService.java
@@ -1,5 +1,6 @@
 package org.akinyemi.tobi.example.services;
 
+import lombok.extern.slf4j.Slf4j;
 import org.akinyemi.tobi.example.ExampleServiceGrpc;
 import org.lognet.springboot.grpc.GRpcService;
 import org.springframework.security.access.annotation.Secured;
@@ -8,10 +9,13 @@ import com.google.protobuf.Empty;
 
 import io.grpc.stub.StreamObserver;
 
+@Slf4j
 @GRpcService
 public class ExampleService extends ExampleServiceGrpc.ExampleServiceImplBase {
     @Secured("SCOPE_monitoring:consent:write")
-    @Override public void endpoint(Empty request, StreamObserver<Empty> responseObserver) {
+    @Override
+    public void endpoint(Empty request, StreamObserver<Empty> responseObserver) {
+        log.info("endpoint reached - doing nothing");
         responseObserver.onNext(Empty.newBuilder().build());
         responseObserver.onCompleted();
     }
