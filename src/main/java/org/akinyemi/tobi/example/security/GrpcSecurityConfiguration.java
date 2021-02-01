1db191ac1b9375f5a16711dbbcbb5855c3ff3462
tobi-yapily <tobi.akinyemi@yapily.com>
2021-02-01 10:20:14 +0000

    simplified security config

--- src/main/java/org/akinyemi/tobi/example/security/GrpcSecurityConfiguration.java
+++ src/main/java/org/akinyemi/tobi/example/security/GrpcSecurityConfiguration.java
@@ -4,14 +4,17 @@ import org.lognet.springboot.grpc.security.EnableGrpcSecurity;
 import org.lognet.springboot.grpc.security.GrpcSecurity;
 import org.lognet.springboot.grpc.security.GrpcSecurityConfigurerAdapter;
 import org.lognet.springboot.grpc.security.jwt.JwtAuthProviderFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtDecoders;
 
-import lombok.RequiredArgsConstructor;
-
-@RequiredArgsConstructor
 @EnableGrpcSecurity
 public class GrpcSecurityConfiguration extends GrpcSecurityConfigurerAdapter {
     private final JwtDecoder jwtDecoder;
+
+    public GrpcSecurityConfiguration(@Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}") String issuer) {
+        jwtDecoder = JwtDecoders.fromOidcIssuerLocation(issuer);
+    }
 
     @Override
     public void configure(GrpcSecurity builder) throws Exception {
